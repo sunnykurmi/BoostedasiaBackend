c423594dd55862c2f05333eab53e4b0e0f1cddce
sunny kurmi <sunnykurmiskks@gmail.com>
2024-08-08 12:05:41 +0530

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const { generatedErrors } = require("./middlewares/errors");
 app.use(logger("tiny"));
 
-const allowedOrigins = ["http://localhost:5173", "https://boostedasia.org/"];
+const allowedOrigins = ["http://localhost:5173", "https://boostedasia.org"];
 app.use(
   cors({
     origin: function (origin, callback) {
