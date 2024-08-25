3845d728ee11c2a39f3e3c1ec41d15909d70f929
sunny kurmi <sunnykurmiskks@gmail.com>
2024-08-25 21:48:38 +0530

    🎉🎉

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -1,7 +1,12 @@
 exports.sendtoken = (user, statuscode, res) => {
+    console.log("user   ", user, "\n status code   ", statuscode);
     const token = user.getjwttoken();
+    console.log("token    ", token);
+
     const cookieExpireDays = parseInt(process.env.COOKIE_EXPIRE);
+    console.log("expire time for cookie   ", cookieExpireDays);
     if (isNaN(cookieExpireDays) || cookieExpireDays <= 0) {
+        console.log("Invalid COOKIE_EXPIRE configuration");
         return res.status(500).json({ message: 'Invalid COOKIE_EXPIRE configuration' });
     }
     const expirationDate = new Date();
@@ -9,6 +14,11 @@ exports.sendtoken = (user, statuscode, res) => {
     const options = {
         expires: expirationDate,
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Set to true in production
+        sameSite: "None", // Necessary for cross-origin cookies
     };
-    res.status(statuscode).cookie('token', token, options).json({ success: true, id: user._id, token });
+    console.log("cookie options.... ", options);
+    res.status(statuscode)
+        .cookie('token', token, options)
+        .json({ success: true, id: user._id, token });
 };
